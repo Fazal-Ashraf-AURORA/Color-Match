3854a4470d557e3490243bbef08e3860fbb9df38
Fazal Ashraf <fazalashrafofficial@gmail.com>
2023-12-03 07:51:39 +0530

    Removed Difficulty

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,16 +3,15 @@ using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
     public TextMeshProUGUI finalScoreUI;
     public static  bool isGameOver = false;
     public static int SCORE = 0;
     [SerializeField] CollisionHandler collisionHandler;
-    //[SerializeField] ShapeController[] shapeController;
-    [SerializeField]private float currentSpeed; // The current speed of the game.
 
     [SerializeField] GameObject gameOverUI;
-
-    public float baseSpeed = 1.0f; // The base speed of the game.
     private int previousScore = 0; // The score from the previous frame.
 
     void Start() {
@@ -22,25 +21,12 @@ public class GameManager : MonoBehaviour {
       
         Application.targetFrameRate = 60;
         
-        // Initialize the current speed to the base speed.
-        currentSpeed = baseSpeed;
     }
 
     void Update() {
 
         // Get the current score from your score manager.
         int currentScore = SCORE;
-
-        // Check if the score has increased by 10 or more since the last frame.
-        if (currentScore >= previousScore + 5) {
-            // Increase the speed by a certain factor. You can adjust this to your liking.
-            currentSpeed += 1.1f;
-
-            Time.timeScale = currentSpeed;
-
-            // Update the previous score.
-            previousScore = currentScore;
-        }
 
         if(isGameOver) {
             GameOver();
--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -1,26 +1,19 @@
 using UnityEngine;
 
-public class ShapeGenerator : MonoBehaviour
-{
-    public GameObject[] objectsToSpawn; // Array of prefabs to spawn
-    public Vector2 spawnRange = new Vector2(-5f, 5f); // Range where objects will be spawned
-    public float spawnDelay = 2f; // Time delay between spawns
+public class ShapeGenerator : MonoBehaviour {
+    public GameObject[] shapesToSpawn;
+    private float spawnDelay = 2f;
 
-    void Start() {
-        // Start spawning objects with a delay
+    private void Start() {
+        // InvokeRepeating calls the SpawnObject method with the specified delay and repeat rate
         InvokeRepeating("SpawnObject", 0f, spawnDelay);
     }
 
+
     void SpawnObject() {
-        // Generate a random index to select a prefab from the array
-        int randomIndex = Random.Range(0, objectsToSpawn.Length);
-        GameObject objectPrefab = objectsToSpawn[randomIndex];
+        int randomIndex = Random.Range(0, shapesToSpawn.Length);
+        GameObject shapePrefab = shapesToSpawn[randomIndex];
 
-        //// Generate a random position within the specified range
-        //float randomX = Random.Range(spawnRange.x, spawnRange.y);
-        //Vector3 spawnPosition = new Vector3(randomX, transform.position.y, 0f);
-
-        // Instantiate the randomly selected object at the random position
-        Instantiate(objectPrefab, transform.position, Quaternion.identity);
+        Instantiate(shapePrefab, transform.position, Quaternion.identity);
     }
 }
